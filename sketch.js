72fa328ab3fded99dc6cb9937e160562c00da4d4
IsabelleCardenas <00001107270388sp@aluno.educacao.sp.gov.br>
2024-08-20 14:40:28 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600,600);
+  background("blACK");
+  }
+  
+  function draw() {
+    stroke("white");
+    fill("khaki");
+  
+  if(mouseIsPressed){
+    rect(mouseX,mouseY, 20, 35);
+   }
+    
+  }
+  
